779a82ccce219e163f2afe2452da91ee85c6611e
ElinSabel <elin.sabel@live.se>
2026-01-08 14:14:16 +0100

    environmental variable changed

--- frontend/src/services/baseService.ts
+++ frontend/src/services/baseService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 
-export const API_URL = "http://localhost:3000";
+export const API_URL = "https://live-flakbygg-examensarbete-backend.vercel.app/";
 
 export const api = axios.create({
   baseURL: API_URL,
