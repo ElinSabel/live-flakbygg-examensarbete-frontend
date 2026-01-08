2a2794dfdcf2b6da3eda487dbb29440cf35c9395
ElinSabel <elin.sabel@live.se>
2026-01-08 16:42:03 +0100

    test

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router";
 import { About } from "./pages/About";
 import { CustomerDashboard } from "./pages/customer/CustomerDashboard";
 import { CustomerOrder } from "./pages/customer/CustomerOrder";
